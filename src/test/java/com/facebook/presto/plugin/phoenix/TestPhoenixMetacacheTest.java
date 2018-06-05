eda96a9a616f3ef1dfd663ba36e7dc71b0065537
Byunghwa Yun <combine@combineads.co.kr>
2018-06-05 17:16:37 +0900

    Add dynamic column cache.

--- src/test/java/com/facebook/presto/plugin/phoenix/TestPhoenixMetacacheTest.java
+++ src/test/java/com/facebook/presto/plugin/phoenix/TestPhoenixMetacacheTest.java
@@ -18,7 +18,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.testng.annotations.Test;
 
-@Test
+@Test(singleThreaded = true)
 public class TestPhoenixMetacacheTest
         extends AbstractTestIntegrationSmokeTest
 {
@@ -28,8 +28,8 @@ public class TestPhoenixMetacacheTest
         super(() -> PhoenixQueryRunner.createPhoenixQueryRunner(4, ImmutableMap.of(), ImmutableList.of()));
     }
 
-    @Test
-    public void tesdtNoneMetaCacahe() throws InterruptedException
+    @Test(invocationCount = 5)
+    public void tesdtMetaCacahe() throws InterruptedException
     {
         String tableName = "TEST_META_CACHE";
         assertUpdate("CREATE TABLE " + tableName + "(ENTRY VARCHAR, DUMMY1 VARCHAR, DUMMY2 VARCHAR) WITH (ROWKEYS = ARRAY['ENTRY'], UPDATE_CACHE_FREQUENCY=0)");
@@ -40,6 +40,38 @@ public class TestPhoenixMetacacheTest
         assertUpdate("CREATE TABLE " + tableName + "(ENTRY VARCHAR, DUMMY1 BIGINT, DUMMY2 BIGINT) WITH (ROWKEYS = ARRAY['ENTRY'], UPDATE_CACHE_FREQUENCY=0)");
         assertUpdate("INSERT INTO " + tableName + " VALUES('KEY1', 2000, 3000)", 1);
         assertQuery("SELECT ENTRY, DUMMY1, DUMMY2 FROM " + tableName + " where ENTRY = 'KEY1'", "SELECT 'KEY1', 2000, 3000");
+        assertUpdate("DROP TABLE " + tableName + "");
+    }
+
+    @Test(invocationCount = 5)
+    public void tesdtDifferentColumnNameMetaCacahe() throws InterruptedException
+    {
+        String tableName = "TEST_META_CACHE";
+        assertUpdate("CREATE TABLE " + tableName + "(ENTRY VARCHAR, DUMMY1 VARCHAR, DUMMY2 VARCHAR) WITH (ROWKEYS = ARRAY['ENTRY'], UPDATE_CACHE_FREQUENCY=0)");
+        assertUpdate("INSERT INTO " + tableName + " VALUES('KEY1', 'DUMMYVALUE1', '3000')", 1);
+        assertQuery("SELECT ENTRY, DUMMY1, DUMMY2 FROM " + tableName + " where ENTRY = 'KEY1'", "SELECT 'KEY1', 'DUMMYVALUE1', '3000'");
+        assertUpdate("DROP TABLE " + tableName + "");
+
+        assertUpdate("CREATE TABLE " + tableName + "(ENTRY VARCHAR, DUMMY3 BIGINT, DUMMY4 BIGINT) WITH (ROWKEYS = ARRAY['ENTRY'], UPDATE_CACHE_FREQUENCY=0)");
+        assertUpdate("INSERT INTO " + tableName + " VALUES('KEY1', 2000, 3000)", 1);
+        assertQuery("SELECT ENTRY, DUMMY3, DUMMY4 FROM " + tableName + " where ENTRY = 'KEY1'", "SELECT 'KEY1', 2000, 3000");
+        assertUpdate("DROP TABLE " + tableName + "");
+    }
+
+    @Test(invocationCount = 5)
+    public void tesdtDynamicColumnNameMetaCacahe() throws InterruptedException
+    {
+        String tableName = "TEST_META_CACHE";
+        String dynamicTableName = "\"TEST_META_CACHE$DCOL1 VARCHAR,DCOL2 VARCHAR\"";
+        assertUpdate("CREATE TABLE " + tableName + "(ENTRY VARCHAR, DUMMY1 VARCHAR, DUMMY2 VARCHAR) WITH (ROWKEYS = ARRAY['ENTRY'], UPDATE_CACHE_FREQUENCY=0)");
+        assertUpdate("INSERT INTO " + dynamicTableName + " VALUES('KEY1', 'DUMMYVALUE1', '3000', 'DCOLVALUE1', 'DCOLVALUE2')", 1);
+        assertQuery("SELECT ENTRY, DUMMY1, DUMMY2, DCOL1, DCOL2 FROM " + dynamicTableName + " where ENTRY = 'KEY1'", "SELECT 'KEY1', 'DUMMYVALUE1', '3000', 'DCOLVALUE1', 'DCOLVALUE2'");
+        assertUpdate("DROP TABLE " + tableName + "");
+
+        dynamicTableName = "\"TEST_META_CACHE$DCOL1 BIGINT,DCOL2 BIGINT\"";
+        assertUpdate("CREATE TABLE " + tableName + "(ENTRY VARCHAR, DUMMY3 BIGINT, DUMMY4 BIGINT) WITH (ROWKEYS = ARRAY['ENTRY'], UPDATE_CACHE_FREQUENCY=0)");
+        assertUpdate("INSERT INTO " + dynamicTableName + " VALUES('KEY1', 2000, 3000, 4000, 5000)", 1);
+        assertQuery("SELECT ENTRY, DUMMY3, DUMMY4, DCOL1, DCOL2 FROM " + dynamicTableName + " where ENTRY = 'KEY1'", "SELECT 'KEY1', 2000, 3000, 4000, 5000");
         assertUpdate("DROP TABLE " + tableName + "");
     }
 }
